c4810257d0486d47a70660850782f783c22b9409
Cankut ER <cnkt.er@gmail.com>
2023-06-27 11:15:22 +0300

    initial

--- lib/lambda_versioning-stack.ts
+++ lib/lambda_versioning-stack.ts
@@ -1,16 +1,17 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
 
 export class LambdaVersioningStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const helloLambda = new lambda.Function(this, "helloLambda", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "hello.handler",
+      currentVersionOptions: { removalPolicy: cdk.RemovalPolicy.RETAIN },
+    });
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaVersioningQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    helloLambda.addAlias("lastVersion");
   }
 }
