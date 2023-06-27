ae8b23f64e4e7d8a9e2a6ae430be74e6521f2c1e
Cankut ER <cnkt.er@gmail.com>
2023-06-27 10:43:06 +0300

    Initial commit

---
+++ test/lambda_versioning.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaVersioning from '../lib/lambda_versioning-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda_versioning-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaVersioning.LambdaVersioningStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
